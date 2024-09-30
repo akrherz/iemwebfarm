b9b7cb9311bdd818064df85bf230d3e2c8ecaee8
akrherz <akrherz@iastate.edu>
2024-09-29 22:50:28 -0500

    ✅ Update things

--- scripts/f5util.sh
+++ scripts/f5util.sh
@@ -10,3 +10,9 @@ iptables $FLAG INPUT -s 10.90.15.253 -j DROP
 iptables $FLAG INPUT -s 10.90.15.252 -j DROP
 ip6tables $FLAG INPUT -s 2610:130:108:83::252 -j DROP
 ip6tables $FLAG INPUT -s 2610:130:108:83::253 -j DROP
+
+# Unsure attm
+iptables $FLAG INPUT -s 129.186.6.66 -j DROP
+iptables $FLAG INPUT -s 129.186.6.67 -j DROP
+ip6tables $FLAG INPUT -s 2610:130:108:805::81ba:642 -j DROP
+ip6tables $FLAG INPUT -s 2610:130:108:805::81ba:643 -j DROP
