001b8cfdfe7b15d55924dfbcfd93eff59b65cd78
akrherz <akrherz@iastate.edu>
2024-10-11 19:34:07 -0500

    ✨ App firewall updates

--- scripts/process_block_queue.py
+++ scripts/process_block_queue.py
@@ -25,28 +25,23 @@ def main(argv):
     )
     cursor = pgconn.cursor()
     cursor.execute(
-        "SELECT ctid, protocol, client_addr from weblog_block_queue WHERE "
+        "SELECT ctid, x_forwarded_for from weblog_block_queue WHERE "
         "target = %s",
         (myname,),
     )
+    updated = False
     for row in cursor:
-        exe = "iptables" if row[1] == 4 else "ip6tables"
-        cmd = [
-            f"/usr/sbin/{exe}",
-            "-I",
-            "INPUT",
-            "-s",
-            row[2],
-            "-j",
-            "DROP",
-        ]
-        subprocess.call(cmd)
+        updated = True
+        with open("/etc/httpd/conf.d/blocklist", "a") as fh:
+            fh.write(f"SetEnvIf X-Forwarded-For ^{row[1]}$ BlockAccess\n")
         cursor2 = pgconn.cursor()
         cursor2.execute(
             "DELETE from weblog_block_queue where ctid = %s", (row[0],)
         )
         cursor2.close()
         pgconn.commit()
+    if updated:
+        subprocess.call(["systemctl", "reload", "httpd"])
 
 
 if __name__ == "__main__":
