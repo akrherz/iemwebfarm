aaddfe08967f2b364f1a18cbf0bd907a2db6dc33
akrherz <akrherz@iastate.edu>
2026-06-18 11:04:26 -0500

    💚 Address some codereview

--- scripts/forklift.sh
+++ scripts/forklift.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # Sometimes we need to forklift production, so this attempts to do it nicely.
+set -e
 set -x
 HOST_PATTERN="iemwebfarm,iem_appliance,anticyclone.agron.iastate.edu"
 INVENTORY_DIR="/home/akrherz/projects/infra-ansible/inventories"
 
 for machine in $(ansible $HOST_PATTERN -i $INVENTORY_DIR --list-hosts | awk 'NR>1 {print $1}'); do
   # Enable the F5 denial script
-  ssh root@$machine "sh /opt/iemwebfarm/scripts/f5util.sh ON"
+  ssh "root@$machine" "sh /opt/iemwebfarm/scripts/f5util.sh ON"
   # Wait a minute for load to bleed away
   sleep 60
   # Copy miniconda
-  rsync -a -H --delete /opt/miniconda3 mesonet@${machine}:/opt/
+  rsync -a -H --delete /opt/miniconda3 "mesonet@${machine}:/opt/"
   # git pull various repos
-  ssh mesonet@$machine "cd /opt/iem; git pull; cd /opt/iemwebfarm; git pull"
+  ssh "mesonet@$machine" "cd /opt/iem && git pull && cd /opt/iemwebfarm && git pull"
   # Restart apache
-  ssh root@$machine "systemctl restart httpd"
+  ssh "root@$machine" "systemctl restart httpd"
   # Stop F5 denial
-  ssh root@$machine "sh /opt/iemwebfarm/scripts/f5util.sh OFF"
+  ssh "root@$machine" "sh /opt/iemwebfarm/scripts/f5util.sh OFF"
 done
